ff3a306b485b4965d7918c9ac58286d4f2fc4b35
Justin Lawson <jstn.lawson@gmail.com>
2024-02-10 16:15:39 -0600

    footer html css

--- app/wp-content/themes/lasso/functions.php
+++ app/wp-content/themes/lasso/functions.php
@@ -2,33 +2,33 @@
 
 error_log('functions.php is loaded');
 
-function lasso_add_css() {
+function gradient_add_css() {
 
     // register the style
-    wp_register_style( 'lasso-main-css', get_template_directory_uri() . '/assets/css/main.css');
+    wp_register_style( 'gradient-main-css', get_template_directory_uri() . '/assets/css/main.css');
 
     // enqueue the style
-    wp_enqueue_style( 'lasso-main-css' );
+    wp_enqueue_style( 'gradient-main-css' );
 
     // enqueue the style conditionally
     // if ( is_front_page() ) {
         
-    //     wp_enqueue_style( 'lasso-main-css' );
+    //     wp_enqueue_style( 'gradient-main-css' );
         
     // }
 }
 
-add_action( 'wp_enqueue_scripts', 'lasso_add_css' );
+add_action( 'wp_enqueue_scripts', 'gradient_add_css' );
 
-function lasso_theme_widgets_init() {
+function gradient_theme_widgets_init() {
     register_sidebar( array(
-        'name'          => esc_html__( 'Lasso Carousel Section', 'lasso' ),
-        'id'            => 'lasso-carousel-section',
-        'description'   => esc_html__( 'Add widgets here.', 'lasso' ),
+        'name'          => esc_html__( 'gradient Carousel Section', 'gradient' ),
+        'id'            => 'gradient-carousel-section',
+        'description'   => esc_html__( 'Add widgets here.', 'gradient' ),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
         'before_title'  => '<h2 class="widget-title">',
         'after_title'   => '</h2>',
     ) );
 }
-add_action( 'widgets_init', 'lasso_theme_widgets_init' );
+add_action( 'widgets_init', 'gradient_theme_widgets_init' );
--- app/wp-content/themes/lasso/index.php
+++ app/wp-content/themes/lasso/index.php
@@ -3,32 +3,58 @@
 
 
 <head>
-    <title>Lasso</title>
+    <title>gradient</title>
     <?php wp_head(); ?>
 </head>
 
 <body>
+    <div id="wpgt">
+        <div class="gradient-header">
+            <header>
+                <div class="gradient-header__title">
+                    <h1>lasso</h1>
+                </div>
+                <div class='gradient-nav'>
+                    <nav>
+                        <ul>
+                            <li><a href="#">products</a></li>
+                            <li><a href="#">in stock</a></li>
+                            <li><a href="#">custom</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </div>
+        <div class="gradient-header__spacer"></div>
+        <div class="gradient-gradient">
+            <div class="gradient-section">
+                <?php if (is_active_sidebar('gradient-carousel-section')) : ?>
+                    <?php dynamic_sidebar('gradient-carousel-section'); ?>
+                <?php endif; ?>
+            </div>
 
-    <header class="lasso-header">
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-    <div class="lasso-header__spacer"></div>
-    <div class="lasso-main">
-        <div class="lasso-section">
-            <?php if (is_active_sidebar('lasso-carousel-section')) : ?>
-                <?php dynamic_sidebar('lasso-carousel-section'); ?>
-            <?php endif; ?>
+
+            <footer class="gradient-footer">
+                <div class="gradient-footer__company">
+                    <span class="gradient-footer__company--logo">l</span>
+                    <p class="gradient-footer__company--est">&copy; 2024 lasso inc</p>
+                </div>
+                <div class="gradient-footer__contact">
+                    <h4 class="gradient-footer__title">contact</h4>
+                    <ul>
+                        <li>lasso@lasso.com</li>
+                    </ul>
+                </div>
+                <div class="gradient-footer__social">
+                    <h4 class="gradient-footer__title">follow us</h4>
+                    <ul>
+                        <li><a href="#">facebook</a></li>
+                    </ul>
+                </div>
+
+            </footer>
         </div>
     </div>
-    <footer class="lasso-footer">
-        <p>&copy; 2021 Lasso</p>
-    </footer>
     <?php wp_footer(); ?>
 </body>
 
