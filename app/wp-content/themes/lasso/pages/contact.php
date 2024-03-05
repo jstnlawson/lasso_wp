516fe82320946b828cb02336d08049de6ab97323
Justin Lawson <jstn.lawson@gmail.com>
2024-03-05 11:09:25 -0600

    contact wrapper

--- app/wp-content/themes/lasso/pages/contact.php
+++ app/wp-content/themes/lasso/pages/contact.php
@@ -5,12 +5,20 @@ Template Name: Contact
 */
 get_header(); ?>
 <div class="page-spacer"></div>
-<?php
-if ( have_posts() ) : while ( have_posts() ) : the_post();
+<div class="contact">
+    <div class="contact-banner">
+        <h1 class="contact-banner__title">Contact Us</h1>
+        <p class="contact-banner__text">We'd love to hear from you. Please fill out the form below and we'll get back to you as soon as possible.</p>
+    </div>
+    <div class="form-container">
+        <?php
+        if (have_posts()) : while (have_posts()) : the_post();
 
-    the_content();
+                the_content();
 
-endwhile; endif;
-?>
-
+            endwhile;
+        endif;
+        ?>
+    </div>
+</div>
 <?php get_footer() ?>
